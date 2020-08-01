b17a26646b7296bc0c1fe6355a9d44014d45bb89
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-08-02 00:00:23 +0900

    FFT 弱高速化

--- Mathematics/Convolution/FastFourierTransform_Cooley-Tukey-time.cpp
+++ Mathematics/Convolution/FastFourierTransform_Cooley-Tukey-time.cpp
@@ -12,7 +12,8 @@ N を 2 冪として f[0], f[1], \ldots, f[N - 1] が既知
 
 \omega_N := 1 の原始 N 乗根
 
-F[\omega_N^{-i}] := \Sum_{k = 0}^{N - 1} f[k] \omega_N^{-ki}
+F[z] := \Sum_{k = 0}^{N - 1} f[k] z^k
+F[\omega_N^{-i}] = \Sum_{k = 0}^{N - 1} f[k] \omega_N^{-ki}
 で偶数次数、奇数次数で多項式を分ける
 
 0 \leq i < N/2 として
@@ -43,19 +44,21 @@ public:
 	static std::vector<value_type> multiply(const std::vector<T> &A, const std::vector<T> &B) {
 		if (A.empty() || B.empty()) return {};
 		size_type n_ = A.size() + B.size() - 1;
-		size_type n = 1;
-		while (n < n_) n <<= 1;
+		size_type n = 1, ni = 0;
+		while (n < n_) n <<= 1, ++ni;
+		const std::vector<complex_type> zeta = _zeta(ni);
+		
 		std::vector<complex_type> a, b;
 		a.reserve(n), b.reserve(n);
 		for (size_type i = 0; i < A.size(); ++i) a.emplace_back(A[i], 0);
 		for (size_type i = 0; i < B.size(); ++i) b.emplace_back(B[i], 0);
-		a.resize(n); fft(a);
-		b.resize(n); fft(b);
+		a.resize(n); fft(a, zeta);
+		b.resize(n); fft(b, zeta);
 		
 		std::vector<complex_type> c;
 		c.reserve(n);
 		for (size_type i = 0; i < n; ++i) c.emplace_back(std::conj(a[i] * b[i]));
-		fft(c);
+		fft(c, zeta);
 		
 		std::vector<value_type> res;
 		res.reserve(n);
@@ -64,22 +67,19 @@ public:
 	}
 	
 private:
-	static void fft(std::vector<complex_type> &A) {
+	static void fft(std::vector<complex_type> &A, const std::vector<complex_type> &zeta) {
 		const size_type N = A.size();
 		const value_type PI = std::acos(static_cast<value_type>(-1));
 		
+		size_type zi = 0;
+		for (size_type i = 1; i < zeta.size(); i <<= 1, ++zi);
+		
 		bit_reverse(A);
-		for (size_type n = 2; n <= N; n <<= 1) {
+		for (size_type n = 2; n <= N; n <<= 1, --zi) {
 			const size_type m = n >> 1;
-			std::vector<complex_type> zeta;
-			zeta.reserve(m);
-			zeta.emplace_back(1);
-			const complex_type zeta0 = std::polar<value_type>(1, static_cast<value_type>(-2) * PI / static_cast<value_type>(n));
-			for (size_type i = 1; i < m; ++i) zeta.emplace_back(zeta.back() * zeta0);
-			
 			for (size_type p = 0; p < N; p += n) {
 				for (size_type i = p, ei = p + m; i != ei; ++i) {
-					const complex_type a = A[i], b = A[i + m] * zeta[i - p];
+					const complex_type a = A[i], b = A[i + m] * zeta[(i - p) << zi];
 					A[i] = a + b;
 					A[i + m] = a - b;
 				}
@@ -93,5 +93,20 @@ private:
 			for (size_type k = N >> 1; k > (j ^= k); k >>= 1);
 			if (i < j) std::swap(A[i], A[j]);
 		}
+	}
+	
+	static std::vector<complex_type> _zeta(size_type max_p) {
+		const value_type PI = std::acos(static_cast<value_type>(-1));
+		// zeta[i] := \omega_{2^max_p}^j (0 \leq j < 2^(i - 1))
+		std::vector<complex_type> zeta(1 << max_p - 1);
+		zeta[0] = complex_type(1, 0);
+		for (size_type i = 0; i < max_p - 1; ++i) {
+			const value_type rad = static_cast<value_type>(-2) * PI / static_cast<value_type>(1 << max_p - i);
+			zeta[1 << i] = std::polar<value_type>(1, rad);
+			for (size_type j = (1 << i) + 1, ej = 1 << i + 1; j != ej; ++j) {
+				zeta[j] = zeta[1 << i ^ j] * zeta[1 << i];
+			}
+		}
+		return zeta;
 	}
 };
--- Mathematics/Convolution/FastFourierTransform_Cooley-Tukey.cpp
+++ Mathematics/Convolution/FastFourierTransform_Cooley-Tukey.cpp
@@ -31,19 +31,21 @@ public:
 	static std::vector<value_type> multiply(const std::vector<T> &A, const std::vector<T> &B) {
 		if (A.empty() || B.empty()) return {};
 		size_type n_ = A.size() + B.size() - 1;
-		size_type n = 1;
-		while (n < n_) n <<= 1;
+		size_type n = 1, ni = 0;
+		while (n < n_) n <<= 1, ++ni;
+		const std::vector<complex_type> zeta = _zeta(ni);
+		
 		std::vector<complex_type> a, b;
 		a.reserve(n), b.reserve(n);
 		for (size_type i = 0; i < A.size(); ++i) a.emplace_back(A[i], 0);
 		for (size_type i = 0; i < B.size(); ++i) b.emplace_back(B[i], 0);
-		a.resize(n); fft_frequency(a);
-		b.resize(n); fft_frequency(b);
+		a.resize(n); fft_frequency(a, zeta);
+		b.resize(n); fft_frequency(b, zeta);
 		
 		std::vector<complex_type> c;
 		c.reserve(n);
 		for (size_type i = 0; i < n; ++i) c.emplace_back(std::conj(a[i] * b[i]));
-		fft_time(c);
+		fft_time(c, zeta);
 		
 		std::vector<value_type> res;
 		res.reserve(n);
@@ -52,47 +54,58 @@ public:
 	}
 	
 private:
-	static void fft_frequency(std::vector<complex_type> &A) {
+	static void fft_frequency(std::vector<complex_type> &A, const std::vector<complex_type> &zeta) {
 		const size_type N = A.size();
 		const value_type PI = std::acos(static_cast<value_type>(-1));
 		
-		for (size_type n = N; n > 1; n >>= 1) {
+		size_type zi = 1;
+		for (size_type i = 1; i < zeta.size(); i <<= 1, ++zi);
+		size_type ni = zi;
+		while (1 << ni > A.size()) --ni;
+		
+		for (size_type n = N; n > 1; n >>= 1, --ni) {
 			const size_type m = n >> 1;
-			std::vector<complex_type> zeta;
-			zeta.reserve(m);
-			zeta.emplace_back(1);
-			const complex_type zeta0 = std::polar<value_type>(1, static_cast<value_type>(-2) * PI / static_cast<value_type>(n));
-			for (size_type i = 1; i < m; ++i) zeta.emplace_back(zeta.back() * zeta0);
-			
 			for (size_type p = 0; p < N; p += n) {
 				for (size_type i = p, ei = p + m; i != ei; ++i) {
 					const complex_type a = A[i], b = A[i + m];
 					A[i] = a + b;
-					A[i + m] = (a - b) * zeta[i - p];
+					A[i + m] = (a - b) * zeta[(i - p) << (zi - ni)];
 				}
 			}
 		}
 	}
 	
-	static void fft_time(std::vector<complex_type> &A) {
+	static void fft_time(std::vector<complex_type> &A, const std::vector<complex_type> &zeta) {
 		const size_type N = A.size();
 		const value_type PI = std::acos(static_cast<value_type>(-1));
 		
-		for (size_type n = 2; n <= N; n <<= 1) {
+		size_type zi = 0;
+		for (size_type i = 1; i < zeta.size(); i <<= 1, ++zi);
+		
+		for (size_type n = 2; n <= N; n <<= 1, --zi) {
 			const size_type m = n >> 1;
-			std::vector<complex_type> zeta;
-			zeta.reserve(m);
-			zeta.emplace_back(1);
-			const complex_type zeta0 = std::polar<value_type>(1, static_cast<value_type>(-2) * PI / static_cast<value_type>(n));
-			for (size_type i = 1; i < m; ++i) zeta.emplace_back(zeta.back() * zeta0);
-			
 			for (size_type p = 0; p < N; p += n) {
 				for (size_type i = p, ei = p + m; i != ei; ++i) {
-					const complex_type a = A[i], b = A[i + m] * zeta[i - p];
+					const complex_type a = A[i], b = A[i + m] * zeta[(i - p) << zi];
 					A[i] = a + b;
 					A[i + m] = a - b;
 				}
 			}
 		}
+	}
+	
+	static std::vector<complex_type> _zeta(size_type max_p) {
+		const value_type PI = std::acos(static_cast<value_type>(-1));
+		// zeta[i] := \omega_{2^max_p}^j (0 \leq j < 2^(i - 1))
+		std::vector<complex_type> zeta(1 << max_p - 1);
+		zeta[0] = complex_type(1, 0);
+		for (size_type i = 0; i < max_p - 1; ++i) {
+			const value_type rad = static_cast<value_type>(-2) * PI / static_cast<value_type>(1 << max_p - i);
+			zeta[1 << i] = std::polar<value_type>(1, rad);
+			for (size_type j = (1 << i) + 1, ej = 1 << i + 1; j != ej; ++j) {
+				zeta[j] = zeta[1 << i ^ j] * zeta[1 << i];
+			}
+		}
+		return zeta;
 	}
 };
