a0e7ebd9c65cb5ecc74399f65fe59aaff774d801
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-08-22 23:32:23 +0900

    RollingHash の追加

---
+++ Test/RollingHash.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_14_B"
+
+#include "String/RollingHash.cpp"
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string T, P;
+	cin >> T >> P;
+	RollingHash rh(T);
+	auto phash = RollingHash(P).hash(0, 0, P.size());
+	
+	for (int i = P.size(); i <= T.size(); ++i) {
+		if (rh.hash(0, i - P.size(), i) == phash) {
+			printf("%d\n", i - P.size());
+		}
+	}
+	
+	return 0;
+}
