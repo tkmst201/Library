75c92cf5f4704450b6d783bc7be4bb2e3d5bd006
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-08-26 23:37:17 +0900

    二部マッチングの高速化と verify

---
+++ Test/BipartiteMatching.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/bipartitematching"
+
+#include "GraphTheory/BipartiteMatching.cpp"
+
+#include <cstdio>
+
+int main() {
+	int L, R, M;
+	scanf("%d %d %d", &L, &R, &M);
+	
+	BipartiteMatching bm(L, R);
+	for (int i = 0; i < M; ++i) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		bm.add_edge(a, b);
+	}
+	bm.build();
+	
+	auto match = bm.get_match();
+	printf("%d\n", match.size());
+	for (int i = 0; i < match.size(); ++i) printf("%d %d\n", match[i].first, match[i].second);
+	
+	return 0;
+}
