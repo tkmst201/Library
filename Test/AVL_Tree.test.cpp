0e522112a3bb4731011aa87d185bcf9142e2b842
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-08-29 16:25:50 +0900

    赤黒木の追加

---
+++ Test/AVL_Tree.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include "DataStructure/AVL_Tree.cpp"
+
+#include <cstdio>
+
+int main() {
+	int Q;
+	scanf("%d", &Q);
+	
+	using ll = long long;
+	using pll = std::pair<ll, ll>;
+	
+	AVL_tree<pll> avl;
+	
+	while (Q--) {
+		int q;
+		ll k, v;
+		scanf("%d %lld", &q, &k);
+		auto it = avl.lower_bound(pll{k, 0});
+		
+		if (q == 0) {
+			scanf("%lld", &v);
+			if (it != avl.end() && it->value.first == k) avl.erase(it);
+			avl.insert(pll{k, v});
+		}
+		else {
+			printf("%lld\n", it == avl.end() || it->value.first != k ? 0 : it->value.second);
+		}
+	}
+	return 0;
+}
---
+++ Test/RedBlackTree.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+
+#include "DataStructure/RedBlackTree.cpp"
+
+#include <cstdio>
+
+int main() {
+	int Q;
+	scanf("%d", &Q);
+	
+	using ll = long long;
+	using pll = std::pair<ll, ll>;
+	
+	RedBlackTree<pll> rbt;
+	
+	while (Q--) {
+		int q;
+		ll k, v;
+		scanf("%d %lld", &q, &k);
+		auto it = rbt.lower_bound(pll{k, 0});
+		
+		if (q == 0) {
+			scanf("%lld", &v);
+			if (it != rbt.end() && it->val.first == k) rbt.erase(it);
+			rbt.insert(pll{k, v});
+		}
+		else {
+			printf("%lld\n", it == rbt.end() || it->val.first != k ? 0 : it->val.second);
+		}
+	}
+	return 0;
+}
