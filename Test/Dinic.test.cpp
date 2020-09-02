4387672f0e488b76d80027e580601d4de61ec916
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-09-02 13:04:59 +0900

    最大フローテストの追加

---
+++ Test/Dinic.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A"
+
+#include "GraphTheory/Dinic.cpp"
+
+#include <cstdio>
+
+int main() {
+	int V, E;
+	scanf("%d %d", &V, &E);
+	Dinic<int> dinic(V);
+	while (E--) {
+		int u, v, c;
+		scanf("%d %d %d", &u, &v, &c);
+		dinic.add_edge(u, v, c);
+	}
+	printf("%d\n", dinic.max_flow(0, V - 1));
+	return 0;
+}
---
+++ Test/EdmondsKarp.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A"
+
+#include "GraphTheory/EdmondsKarp.cpp"
+
+#include <cstdio>
+
+int main() {
+	int V, E;
+	scanf("%d %d", &V, &E);
+	EdmondsKarp<int> ek(V);
+	while (E--) {
+		int u, v, c;
+		scanf("%d %d %d", &u, &v, &c);
+		ek.add_edge(u, v, c);
+	}
+	printf("%d\n", ek.max_flow(0, V - 1));
+	return 0;
+}
