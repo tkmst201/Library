bf089e917813cad44e5cff03e12b2bb8b1e46b94
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-09-05 10:58:50 +0900

    簡潔ビットベクトル, WaveletMatrix の追加(unverified)

---
+++ Test/WaveletMatrix.quantile.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
+
+#include "DataStructure/SuccintBitVector.cpp"
+#include "DataStructure/WaveletMatrix.cpp"
+
+#include <cstdio>
+#include <vector>
+
+int main() {
+	int N, Q;
+	scanf("%d %d", &N, &Q);
+	std::vector<int> a(N);
+	for (int i = 0; i < N; ++i) scanf("%d", &a[i]);
+	WaveletMatrix<30, int, SuccintBitVector> wm(a);
+	while (Q--) {
+		int l, r, k;
+		scanf("%d %d %d", &l, &r, &k);
+		printf("%d\n", wm.quantile(l, r, k + 1));
+	}
+	return 0;
+}
---
+++ Test/WaveletMatrix.range_frequency.test.cpp
@@ -0,0 +1,57 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/challenges/sources/JAG/Summer/2426?year=2012"
+
+#include "DataStructure/SuccintBitVector.cpp"
+#include "DataStructure/WaveletMatrix.cpp"
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+constexpr int INF = 1'000'000'001;
+
+int main() {
+	int N, M;
+	scanf("%d %d", &N, &M);
+	
+	std::vector<int> x(N), y(N), cx, cy;
+	for (int i = 0; i < N; ++i) scanf("%d %d", &x[i], &y[i]);
+	
+	auto compress = [](auto &&v, auto &&cv) -> void {
+		for (auto &&u : v) cv.emplace_back(u);
+		cv.emplace_back(INF);
+		cv.emplace_back(-INF);
+		std::sort(std::begin(cv), std::end(cv));
+		cv.erase(std::unique(std::begin(cv), std::end(cv)), std::end(cv));
+		for (auto &&u : v) u = std::lower_bound(std::begin(cv), std::end(cv), u) - std::begin(cv);
+	};
+	
+	compress(x, cx);
+	compress(y, cy);
+	
+	// 同じ x 座標でも wavelet matrix 上では異なる index になるように修正する必要がある
+	std::vector<int> sum(cx.size()); // [i] := x[j] が i 以下であるような j の個数
+	for (int i = 0; i < x.size(); ++i) ++sum[x[i]];
+	for (int i = 1; i < sum.size(); ++i) sum[i] += sum[i - 1];
+	
+	std::vector<int> cnt(cx.size()), sy(N); // sy[i] := x 座標が i 番目に小さい点の y 座標(x 座標が同一の場合にも差をつける)
+	for (int i = 0; i < x.size(); ++i) sy[sum[x[i] - 1] + cnt[x[i]]++] = y[i];
+	
+	WaveletMatrix<13, int, SuccintBitVector> wm(sy);
+	
+	auto compress_get = [](auto &&cv, auto &&x) {
+		return std::lower_bound(std::begin(cv), std::end(cv), x) - std::begin(cv);
+	};
+	
+	for (int i = 0; i < M; ++i) {
+		int sty, stx, edy, edx;
+		scanf("%d %d %d %d", &stx, &sty, &edx, &edy);
+		++edx; ++edy;
+		sty = compress_get(cy, sty);
+		stx = compress_get(cx, stx);
+		edy = compress_get(cy, edy);
+		edx = compress_get(cx, edx);
+		printf("%d\n", wm.range_frequency(sum[stx - 1], sum[edx - 1], sty, edy));
+	}
+	
+	return 0;
+}
