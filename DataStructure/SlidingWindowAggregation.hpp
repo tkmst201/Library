e687d2361d2fa3f7fa974c652fb70327edd63d0b
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-09-22 11:21:23 +0900

    SWAG の抽象化

--- DataStructure/SlidingWindowAggregation.hpp
+++ DataStructure/SlidingWindowAggregation.hpp
@@ -2,112 +2,93 @@
 #define INCLUDE_GUARD_SLIDING_WINDOW_AGGREGATION_HPP
 
 /*
-last-updated: 2019/12/18
+last-updated: 2020/09/22
 
-現在 区間min のみ
-抽象化したいね
-vbackは1要素しか必要ないのでそうするべき
+# 概要
+モノイドを扱う SWAG
+queue の push, pop に加え、要素全体を入れた順に fold した結果を償却 O(1) で求めることができるデータ構造
 
 # 仕様
-void push(const int &x) :
-	時間計算量: Θ(1) amortized
-	後ろに値 x を入れる
+template<
+	typename T: 扱うモノイド
+>
 
-void pop() :
-	時間計算量: Θ(1) amortized
-	先頭の要素を削除する
-
-bool empty() :
+SlidingWindowAggregation(const_reference id_elem, const F & f)
 	時間計算量: Θ(1)
-	空かどうかを返す
+	単位元 id_elem, 二項演算 f として初期化
 
-int fald_all() :
+size_type size() const noexcept
 	時間計算量: Θ(1)
-	現在入っている全ての要素の演算結果を返す
+	現在入っている要素数を返す
 
-private:
-int func(const int &x, const int &y) :
-	時間計算量: Θ(1)
-	二項演算
+void push(const_reference x)
+	時間計算量: O(1) amortized
+	queue に値 x を追加する
+
+void pop()
+	時間計算量: O(1) amortized
+	queue の最初の要素を削除する
+	必ず要素が存在しなければならない
+
+value_type fold_all()
+	時間計算量: O(1) amortized
+	現在入っている値を fold した結果を返す
 
 # 参考
-https://scrapbox.io/data-structures/Sliding_Window_Aggregation, 2019/12/18
+https://scrapbox.io/data-structures/Sliding_Window_Aggregation, 2020/09/22
+https://snuke.hatenablog.com/entry/2018/09/18/135640, 2020/09/22
 */
 
-#include <stack>
-#include <vector>
+#include <cstdint>
 #include <cassert>
-#include <cstdio>
+#include <vector>
+#include <utility>
+#include <functional>
 
+template<typename T>
 struct SlidingWindowAggregation {
+	using value_type = T;
+	using const_reference = const value_type &;
+	using size_type = std::uint_fast32_t;
+	using F = std::function<value_type(const_reference, const_reference)>;
+	
+private:
+	value_type id_elem;
+	F f;
+	std::vector<std::pair<value_type, value_type>> lstack;
+	std::vector<value_type> rstack;
+	value_type rsum;
+	
 public:
-	void push(const int &x) {
-		push(x, sback, vback);
+	SlidingWindowAggregation(const_reference id_elem, const F & f) : id_elem(id_elem), f(f), rsum(id_elem) {}
+	
+	size_type size() const noexcept { return lstack.size() + rstack.size(); }
+	
+	void push(const_reference x) {
+		rstack.emplace_back(x);
+		rsum = f(rsum, x);
 	}
 	
 	void pop() {
-		assert(!empty());
-		if (sfront.empty()) {
-			while (!sback.empty()) {
-				push(sback.top(), sfront, vfront);
-				sback.pop();
+		assert(size() > 0);
+		if (lstack.empty()) {
+			if (rstack.size() > 1) {
+				lstack.emplace_back(rstack.back(), rstack.back());
+				rstack.pop_back();
 			}
-			vback.clear();
+			while (rstack.size() > 1) {
+				lstack.emplace_back(rstack.back(), f(rstack.back(), lstack.back().second));
+				rstack.pop_back();
+			}
+			rstack.pop_back();
+			rsum = id_elem;
 		}
-		sfront.pop();
-		vfront.pop_back();
+		else lstack.pop_back();
 	}
 	
-	bool empty() {
-		return sfront.empty() && sback.empty();
-	}
-	
-	int fold_all() {
-		assert(!empty());
-		if (vfront.empty()) return vback.back();
-		if (vback.empty()) return vfront.back();
-		return func(vfront.back(), vback.back());
-	}
-	
-private:
-	std::stack<int> sfront, sback;
-	std::vector<int> vfront, vback;
-	
-	void push(const int &x, std::stack<int> &st, std::vector<int> &v) {
-		st.push(x);
-		if (v.empty()) v.push_back(x);
-		else {
-			int addval = func(v.back(), x);
-			v.push_back(addval);
-		}
-	}
-	
-	int func(const int &x, const int &y) {
-		return std::min(x, y);
+	value_type fold_all() const {
+		return f(lstack.empty() ? id_elem : lstack.back().second, rsum);
 	}
 };
-
-
-/*
-// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_3_D
-
-int main() {
-	int N, L;
-	scanf("%d %d", &N, &L);
-	std::vector<int> A(N);
-	for (int i = 0; i < N; ++i) scanf("%d", &A[i]);
-	
-	SlidingWindowAggregation swag;
-	for (int i = 0; i < L - 1; ++i) swag.push(A[i]);
-	
-	for (int i = L - 1; i < N; ++i) {
-		swag.push(A[i]);
-		printf("%d%c", swag.fold_all(), i == N - 1 ? '\n' : ' ');
-		swag.pop();
-	}
-	
-	return 0;
-}
-*/
 
 #endif // INCLUDE_GUARD_SLIDING_WINDOW_AGGREGATION_HPP
---
+++ Test/SlidingWindowAggregation.test.cpp
@@ -0,0 +1,36 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/queue_operate_all_composite"
+
+#include "DataStructure/SlidingWindowAggregation.hpp"
+#include "Mathematics/ModInt.hpp"
+
+#include <cstdio>
+#include <utility>
+
+int main() {
+	int Q;
+	scanf("%d", &Q);
+	
+	using mint = ModInt<998244353>;
+	using P = std::pair<mint, mint>;
+	
+	SlidingWindowAggregation<P> swag({1, 0}, [](auto && x, auto && y) -> P {
+		return {x.first * y.first, x.second * y.first + y.second };
+	});
+	
+	while (Q--) {
+		int q;
+		scanf("%d", &q);
+		if (q == 0) {
+			int a, b;
+			scanf("%d %d", &a, &b);
+			swag.push({a, b});
+		}
+		else if (q == 1) swag.pop();
+		else {
+			int x;
+			scanf("%d", &x);
+			auto res = swag.fold_all();
+			printf("%lld\n", (res.first * x + res.second).get());
+		}
+	}
+}
