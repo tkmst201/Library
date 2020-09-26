79a5dc721a54c42c0aed1c8693e4b9ce493363ca
tkmst201 <57556532+tkmst201@users.noreply.github.com>
2020-09-26 12:14:19 +0900

    完全永続 UnionFind の追加

---
+++ Test/PersistentUnionFind.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/persistent_unionfind"
+
+#include "DataStructure/PersistentArray.hpp"
+#include "DataStructure/PersistentUnionFind.hpp"
+
+#include <cstdio>
+#include <vector>
+
+int main() {
+	int N, Q;
+	scanf("%d %d", &N, &Q);
+	
+	using PUF = PersistentUnionFind<PersistentArray<int, 6>>;
+	std::vector<PUF> hist(Q + 1);
+	
+	for (int i = 0; i < Q; ++i) {
+		int t, k, u, v;
+		scanf("%d %d %d %d", &t, &k, &u, &v);
+		++k;
+		if (t == 0) hist[i + 1] = (hist[k].unite(u, v));
+		else printf("%d\n", hist[k].issame(u, v));
+	}
+}
